bef066c082a3493771fb149afcde2cd628e1e772
yanglong <ambjlon@gmail.com>
2016-04-26 12:06:47 -0400

    rm -fr storage

--- myinstall.sh
+++ myinstall.sh
@@ -1,3 +1,10 @@
 curl -sS https://getcomposer.org/installer | php
 php composer.phar  install
+mkdir storage
+mkdir storage/framework
+mkdir storage/framework/views
+mkdir storage/framework/smarty
+mkdir storage/framework/smarty/cache
+mkdir storage/framework/smarty/compile
+mkdir storage/ss-panel
 chmod -R 777 storage
