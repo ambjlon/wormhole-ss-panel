0b87a3e05c113c021d41a9060d7e909d7a76b7ca
yanglong <ambjlon@163.com>
2016-08-14 09:36:55 +0800

    add import_cardnum_passwd_2_mysql.py

---
+++ tools/import_cardnum_passwd_2_mysql.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import MySQLdb
+
+db = MySQLdb.connect("localhost","root","","shadowsocks")
+cursor = db.cursor()
+pf = open('cardnum_passwd_20160730_halfyear.txt')
+
+line = pf.readline()
+while line:
+    cardnum = line.split(' ')[0]
+    passwd = line.split(' ')[1]
+    sql = 'insert into cardnum_passwd (cardnum, passwd, amount, is_consumed) vales(' + cardnum + ',' + passwd + '30, 0 )';
+    cursor.execute(sql)
+    db.commit()
+    line = pf.readline()
+
+pf.close()
+db.close()
